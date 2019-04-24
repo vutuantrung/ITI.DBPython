d453019e7d2c43109dc4d256eeda242c3090cc1e
VU Tuan Trung <trung.lespace@gmail.com>
2019-04-25 00:34:19 +0200

    Separate functions in main file

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import sqlite3 
 from sqlite3 import Error
+from privateData import dbPath, datPath
 
 def create_connection(db_file):
-    """ create a database connection to a SQLite database""" 
+    """ Create a database connection to a SQLite database""" 
     try:
         conn = sqlite3.connect(db_file)
         print(sqlite3.version)
+        return conn
     except Error as e:
         print(e)
-    finally:
-        conn.close()
+    return None
 
-if __name__ == '__main__':
-    create_connection("E:\\INTECH\\ProgramDB-S8\\Devoir2\\bd\\Exercice2.db")
+def read_source_data():
+    try:
+        with open(datPath, 'r') as file:
+            return file
+    except Error as e:
+        print(e)
+    """
+    with open(datPath, 'r') as file:
+        for line in file:
+            print(line)
+    """
+    return None
 
-        
+def main():
+    conn = create_connection(dbPath)
+    return None
+
