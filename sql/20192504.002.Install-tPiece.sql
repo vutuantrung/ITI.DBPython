bff7bf94848904c7ca1d195ac8b5a3b9b7fcddb7
VU Tuan Trung <trung.lespace@gmail.com>
2019-04-25 00:35:50 +0200

    Add some SQL scripts + modify gitignore

---
+++ sql/20192504.002.Install-tPiece.sql
@@ -0,0 +1,7 @@
+create table if not exists pieces (
+  id_piece integer not null primary key,
+  titre    varchar(30) not null
+           constraint longueur_titre
+                check (length(titre) <= 30),
+  constraint titre_unique unique(titre))
+;
